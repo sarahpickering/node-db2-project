fc69b9777625e83b23421c2ff64d0675235bf093
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-01-10 13:56:56 -0700

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments("id");
+    tbl.string("vin", 17).notNullable().unique();
+    tbl.string("make", 128).notNullabel();
+    tbl.string("model", 5000).notNullabel();
+    tbl.numeric("mileage").unsigned.notNullable();
+    tbl.string("title");
+    tbl.string("transmission");
+  });
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars');
 };
