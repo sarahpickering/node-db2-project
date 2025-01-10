53134713197175a9dfa8a9a44a730d04ea533e39
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-01-10 13:46:43 -0700

    set up dependencies, create cars.js and exports

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,28 @@
-// STRETCH
+const cars = [
+    {
+        vin: '00000000000000000',
+        make: 'gmc',
+        model: 'yukon'
+        mileage: 108000,
+        title: 'clean',
+        transmission: 'automatic'
+    },
+    {
+        vin: '00000000000000000',
+        make: 'honda',
+        model: 'civic'
+        mileage: 43000,
+        title: 'clean',
+    },
+    {
+        vin: '00000000000000000',
+        make: 'dodge',
+        model: 'ram'
+        mileage: 15000,
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
